b105de846a63af15b1c40d1bdba8a570686b3bcc
Lennart Gastler <lennart@lgastler.de>
2022-05-18 16:11:17 +0200

    fix(pannellum): nextjs pannellum fix using patch-package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "pannellum-react/es/pannellum/css/video-js.css";
+import "pannellum-react/es/pannellum/css/pannellum.css";
+import "pannellum-react/es/pannellum/css/style-textInfo.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
